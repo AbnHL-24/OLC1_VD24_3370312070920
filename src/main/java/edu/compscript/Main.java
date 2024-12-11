94f0eadb14cd7f378eac28de135a53bf52a0efb5
AbnHL-24 <abner1269@outlook.com>
2024-12-11 15:47:46 -0600

    Analizador.java confirmado en Main.java

--- src/main/java/edu/compscript/Main.java
+++ src/main/java/edu/compscript/Main.java
@@ -1,7 +1,21 @@
 package edu.compscript;
 
+import edu.compscript.model.analisis.Analizador;
+import edu.compscript.model.analisis.parser;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            String input = "imprimir(1-2*3+10);";
+            Analizador analizador = new Analizador(new BufferedReader(new StringReader(input)));
+            parser p = new parser(analizador);
+            var resultado = p.parse();
+            System.out.println(resultado);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
