a2cf03dcbdf48551b1a3ccc643e518adbb1e5335
AbnHL-24 <abner1269@outlook.com>
2024-12-11 15:03:52 -0600

    Add Generador.java

---
+++ src/main/java/edu/compscript/model/analisis/Generador.java
@@ -0,0 +1,37 @@
+package edu.compscript.model.analisis;
+
+/**
+ * Clase encargada de generar ecompilador del proyecto.
+ */
+public class Generador {
+    public static void main(String[] args) {
+        generadorCompilador();
+    }
+
+    public static void generadorCompilador() {
+        try {
+            String ruta = "src/main/java/edu/compscript/model/analisis/";
+            String rutaLexico = ruta + "Lexico.jflex";
+            String rutaGenerados = ruta + "generados/";
+            String rutaSintactico = ruta + "Sintactico.cup";
+            /*
+            Ruta del archivo de JFlex.
+            -d indica donde se crea el archivo del compilado.
+            Ruta de donde generar el compilado.
+             */
+            String[] flex = {rutaLexico, "-d", ruta};
+            jflex.Main.generate(flex);
+
+            /*
+            destdir establece donde se generará el archivo.
+            Ruta de salida.
+            -parser nombra al archivo compilado.
+            Ruta del archivo cup
+             */
+            String[] cup = {"-destdir", ruta, "-parser", "parser",rutaSintactico};
+            java_cup.Main.main(cup);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
